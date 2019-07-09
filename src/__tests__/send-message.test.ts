ad304a71223566e167977e0a2ab6c181b3fd0f17
Nikita Gusakov <nkt@users.noreply.github.com>
2019-07-09 13:31:43 +0300

    test: add test utils & fix tests

--- src/__tests__/send-message.test.ts
+++ src/__tests__/send-message.test.ts
@@ -6,12 +6,9 @@ import dotenv from 'dotenv';
 import { combineLatest } from 'rxjs';
 import { tap, filter, take } from 'rxjs/operators';
 import Bot, { Peer } from '../index';
+import { beforeAllWithContext, afterAllWithContext, testWithContext } from './test-utils';
 
-let bot1: Bot;
-let bot2: Bot;
-let bot2Peer: Peer;
-
-beforeAll(async () => {
+const context = beforeAllWithContext(async () => {
   dotenv.config();
 
   const env = (name: string): string => {
@@ -24,32 +21,37 @@ beforeAll(async () => {
   };
 
   const endpoints = [env('BOT_ENDPOINT')];
-  bot1 = new Bot({ endpoints, token: env('BOT_TOKEN_FIRST') });
-  bot2 = new Bot({ endpoints, token: env('BOT_TOKEN_SECOND') });
+  const bot1 = new Bot({ endpoints, token: env('BOT_TOKEN_FIRST') });
+  const bot2 = new Bot({ endpoints, token: env('BOT_TOKEN_SECOND') });
 
   const bot2Self = await bot2.getSelf();
   if (!bot2Self.nick) {
     throw new Error('bot2 must have nickname');
   }
 
-  const user = await bot1.findUserByNick(bot2Self.nick);
-  if (user) {
-    bot2Peer = Peer.private(user.id);
-  } else {
-    throw new Error('bot1 not able to find bot2');
-  }
+  const bot2Peer = await bot1
+    .findUserByNick(bot2Self.nick)
+    .then((user) => {
+      if (user) {
+        return Peer.private(user.id);
+      }
+
+      throw new Error('bot1 not able to find bot2');
+    });
+
+  return { bot1, bot2, bot2Peer };
 });
 
-afterAll(() => {
+afterAllWithContext(context, async ({ bot1, bot2 }) => {
   bot1.stop();
   bot2.stop();
 });
 
-test('bot2 should receive message from bot1', () => {
+testWithContext('bot2 should receive message from bot1', context, async ({ bot1, bot2, bot2Peer }) => {
   const text = 'hello, world!';
   const now = Date.now();
 
-  return combineLatest(
+  await combineLatest(
     bot2.subscribeToMessages(),
     bot1.sendText(bot2Peer, text)
   )
---
+++ src/__tests__/test-utils.ts
@@ -0,0 +1,33 @@
+type ContextCallback<T> = (context: T) => Promise<void>;
+
+export function beforeAllWithContext<T>(callback: () => Promise<T>): Promise<T> {
+  return new Promise((resole, reject) => {
+    beforeAll(() => callback().then(resole, reject));
+  });
+}
+
+export function afterAllWithContext<T>(context: Promise<T>, callback: ContextCallback<T>) {
+  afterAll(() => context.then(callback));
+}
+
+export function beforeEachWithContext<T>(callback: () => Promise<T>): Promise<T> {
+  return new Promise((resole, reject) => {
+    beforeEach(() => callback().then(resole, reject));
+  });
+}
+
+export function afterEachWithContext<T>(context: Promise<T>, callback: ContextCallback<T>) {
+  afterEach(() => context.then(callback));
+}
+
+export function describeWithContext<T>(name: string, context: Promise<T>, fn: ContextCallback<T>) {
+  describe(name, async () => fn(await context));
+}
+
+export function itWithContext<T>(name: string, context: Promise<T>, fn: ContextCallback<T>) {
+  it(name, async () => fn(await context));
+}
+
+export function testWithContext<T>(name: string, context: Promise<T>, fn: ContextCallback<T>) {
+  test(name, async () => fn(await context));
+}
